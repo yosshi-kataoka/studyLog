ac99ad3cf3e3b14302a0fba24dbd5c6a882fb6b7
github yosshi-kataoka <github>
2024-02-12 06:01:21 +0900

    読書ログの書籍名の入力を標準入力にしました。

--- book_log.php
+++ book_log.php
@@ -1,7 +1,9 @@
 <?php
 
-echo '書籍名:銀河鉄道の夜' . PHP_EOL;
-echo '著者名:宮沢賢治' . PHP_EOL;
-echo '読書状況:読了' . PHP_EOL;
-echo '評価:5' . PHP_EOL;
-echo '感想:ほんとうの幸せとは何だろうかと考えさせられる作品だった' . PHP_EOL;
+echo '書籍名を入力してください:';
+echo trim(fgets(STDIN)) . PHP_EOL;
+// echo '書籍名:銀河鉄道の夜' . PHP_EOL;
+// echo '著者名:宮沢賢治' . PHP_EOL;
+// echo '読書状況:読了' . PHP_EOL;
+// echo '評価:5' . PHP_EOL;
+// echo '感想:ほんとうの幸せとは何だろうかと考えさせられる作品だった' . PHP_EOL;
