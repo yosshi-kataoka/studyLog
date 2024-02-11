eaf8eaf0212404653c8dfc067450f09d0adf061f
github yosshi-kataoka <github>
2024-02-12 06:31:35 +0900

    変数を使用して読書ログの書籍名を表示するようにしました。

--- book_log.php
+++ book_log.php
@@ -1,7 +1,10 @@
 <?php
 
-echo '書籍名を入力してください:';
-echo trim(fgets(STDIN)) . PHP_EOL;
+echo '書籍名を入力してください:' . PHP_EOL;
+$title =  trim(fgets(STDIN));
+echo '書籍名を登録しました' . PHP_EOL . PHP_EOL;
+echo '読書ログを表示します' . PHP_EOL;
+echo '書籍名:' . $title . PHP_EOL;
 // echo '書籍名:銀河鉄道の夜' . PHP_EOL;
 // echo '著者名:宮沢賢治' . PHP_EOL;
 // echo '読書状況:読了' . PHP_EOL;
