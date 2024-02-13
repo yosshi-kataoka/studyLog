9c8d28d81cbdec312f0234d54436e14d313f4cd2
github yosshi-kataoka <github>
2024-02-14 07:03:40 +0900

    連想配列を使用して、読書ログの登録、表示を作成

--- book_log.php
+++ book_log.php
@@ -1,9 +1,12 @@
 <?php
-$title = '';
-$author = '';
-$status = '';
-$evaluation = '';
-$review = '';
+$bookLogs = [];
+$books = [
+  'title' => '',
+  'author' => '',
+  'status' => '',
+  'evaluation' => '',
+  'review' => '',
+];
 
 while (true) {
   echo '1.読書ログの登録' . PHP_EOL;
@@ -16,24 +19,28 @@ while (true) {
     // 読書ログを登録する
     echo '読書ログを登録してください:' . PHP_EOL;
     echo '書籍名:';
-    $title =  trim(fgets(STDIN));
+    $books['title'] =  trim(fgets(STDIN));
     echo '著者名:';
-    $author =  trim(fgets(STDIN));
+    $books['author'] =  trim(fgets(STDIN));
     echo '読書状況(未読,読んでる,読了):';
-    $status =  trim(fgets(STDIN));
+    $books['status'] =  trim(fgets(STDIN));
     echo '評価:';
-    $evaluation =  trim(fgets(STDIN));
+    $books['evaluation'] =  trim(fgets(STDIN));
     echo '感想:';
-    $review =  trim(fgets(STDIN));
+    $books['review'] =  trim(fgets(STDIN));
+    $bookLogs[] = $books;
     echo '登録が完了しました' . PHP_EOL . PHP_EOL;
   } elseif ($num === '2') {
     // 読書ログを表示する
     echo '読書ログを表示します' . PHP_EOL;
-    echo '書籍名:' . $title . PHP_EOL;
-    echo '著者名:' . $author . PHP_EOL;
-    echo '読書状況:' . $status . PHP_EOL;
-    echo '評価:' . $evaluation . PHP_EOL;
-    echo '感想:' . $review . PHP_EOL;
+    foreach ($bookLogs as $bookLog) {
+      var_export($bookLog) . PHP_EOL;
+    }
+    // echo '書籍名:' . $title . PHP_EOL;
+    // echo '著者名:' . $author . PHP_EOL;
+    // echo '読書状況:' . $status . PHP_EOL;
+    // echo '評価:' . $evaluation . PHP_EOL;
+    // echo '感想:' . $review . PHP_EOL;
   } elseif ($num === '9') {
     //  アプリケーションを終了する
     break;
