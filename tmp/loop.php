fac82f841be57c82643d9bad0bbeaf9a2b5e9c02
github yosshi-kataoka <github>
2024-02-14 12:31:58 +0900

    foreachを使用して配列および連想配列の中身を表示する処理を作成

--- tmp/loop.php
+++ tmp/loop.php
@@ -1,11 +1,16 @@
 <?php
-$i = 0;
+// foreachを使用した配列の各値を2倍にして表示する処理
+$numbers = [1, 2, 3, 4, 5,];
+foreach ($numbers as $number) {
+  echo $number * 2 . PHP_EOL;
+}
 
-while (true) {
-  if ($i <= 50) {
-    echo $i . PHP_EOL;
-    $i += 10;
-  } else {
-    break;
-  }
+// foreachを使用した連想配列の中身をそれぞれ表示する処理
+$currencies = [
+  'japan' => 'yen',
+  'us' => 'dollar',
+  'england' => 'pound',
+];
+foreach ($currencies as $country => $currency) {
+  echo $country . ':' . $currency . PHP_EOL;
 }
