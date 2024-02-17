05a344ea1cb95a221f70cc35e5304ceeee7be26c
github yosshi-kataoka <github>
2024-02-18 06:15:56 +0900

    MySQLの学習のため、既存のデータベースにcompaniesテーブルを作成

---
+++ tmp/create_table_company.sql
@@ -0,0 +1,7 @@
+create table companies (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  establishment_date date,
+  founder VARCHAR(255),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) DEFAULT CHARACTER SET = utf8mb4;
