1aadef3dd8f3ebc65a6baa4e800b4ec3e658ad10
github yosshi-kataoka <github>
2024-03-19 07:13:14 +0900

    .envファイルの読み込み処理をdbConnect()内に移動しました。

--- companies/companies_table.php
+++ companies/companies_table.php
@@ -2,11 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
 function dbConnect(): PDO
 {
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+  $dotenv->load();
   try {
     $pdo = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_DATABASE'] .  '; charset=utf8mb4', $_ENV['DB_USER'],  $_ENV['DB_PASSWORD']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
