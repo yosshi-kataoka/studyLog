9b1014be8831a822d07cdd4bea3d6b35f35dadb4
github yosshi-kataoka <github>
2024-03-25 13:37:33 +0900

    読書ログにてPHPとHTMLの分割のためform.phpを作成、new.phpにてinclude。

--- book_logs/create.php
+++ book_logs/create.php
@@ -22,15 +22,14 @@ function validate($bookLog)
     $errors['status'] = '読書状況を選択してください';
   }
   // 評価のバリデーション
-  if (!is_int($bookLog['evaluation'])) {
+  if ($bookLog['evaluation'] < 1 || $bookLog['evaluation'] > 5)
     $errors['evaluation'] = '評価は1~5の整数を選択してください';
-  } elseif ($bookLog['evaluation'] > 0 || $bookLog['evaluation'] < 6)
-    // 感想のバリデーション
-    if (!mb_strlen($bookLog['review'])) {
-      $errors['review'] = '感想を入力してください';
-    } elseif (mb_strlen($bookLog['review']) > 1000) {
-      $errors['review'] = '感想は1000文字以内で入力してください';
-    }
+  // 感想のバリデーション
+  if (!mb_strlen($bookLog['review'])) {
+    $errors['review'] = '感想を入力してください';
+  } elseif (mb_strlen($bookLog['review']) > 1000) {
+    $errors['review'] = '感想は1000文字以内で入力してください';
+  }
   return $errors;
 }
 
@@ -74,62 +73,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("Location: index.php");
   }
 }
-?>
-<!-- 入力値にエラーがある場合以下の処理 -->
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset=utf-8>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>読書ログの登録</title>
-</head>
-
-<body>
-  <h1>読書ログ</h1>
-  <h2>読書ログの登録</h2>
-  <!-- エラー内容をforeachにて表示させる -->
-  <?php if (count($errors) > 0) : ?>
-    <?php foreach ($errors as $error) : ?>
-      <ul>
-        <li>
-          <?php echo $error; ?>
-        </li>
-      </ul>
-    <?php endforeach; ?>
-  <?php endif; ?>
-  <form action="create.php" method="POST">
-    <div>
-      <label for="title">書籍名</label>
-      <input type="text" id="title" name="title">
-    </div>
-    <div>
-      <label for="author">著者名</label>
-      <input type="text" id="author" name="author">
-    </div>
-    <div>
-      <input type="radio" name="status" id="unread" value="unread" checked>
-      <label for="unread">未読</label>
-    </div>
-    <div>
-      <input type="radio" name="status" id="reading" value="reading">
-      <label for="reading">読んでいる</label>
-    </div>
-    <div>
-      <input type="radio" name="status" id="finish_reading" value="finish_reading">
-      <label for="finish_reading">読了</label>
-    </div>
-    <div>
-      <label for="evaluation">評価（5点）満点の整数</label>
-      <input type="number" id="evaluation" name="evaluation" min="1" max="5" step="1">
-    </div>
-    <div>
-      <label for="review">感想</label>
-      <textarea id="review" name="review" rows="10"></textarea>
-    </div>
-    <button type="submit">登録する</button>
-  </form>
-</body>
-
-</html>
+// 入力値にエラーがある場合以下の処理
+include 'views/form.php';
---
+++ companies/views/form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html la="ja">
+
+<head>
+  <meta charset="utf=8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>会社情報の登録</title>
+</head>
+
+<body>
+  <h1>会社情報の登録</h1>
+  <form action="create.php" method="POST">
+    <?php if (count($errors)) : ?>
+      <ul>
+        <?php foreach ($errors as $error) : ?>
+          <li><?php echo $error; ?></li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+    <div>
+      <label for="name">会社名</label>
+      <input type="text" id="name" name="name">
+    </div>
+    <div>
+      <label for="establishment
+      _date">設立日</label>
+      <input type="date" id="establishment_date" name="establishment_date">
+    </div>
+    <div>
+      <label for="founder">代表者名</label>
+      <input type="text" id="founder" name="founder">
+    </div>
+    </div>
+    <button type="submit">登録する</button>
+  </form>
+</body>
+
+</html>
