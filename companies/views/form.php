78fe2cfdbead04ff12d973d5f20298a1c835fdb9
github yosshi-kataoka <github>
2024-03-28 12:44:46 +0900

    Bootstrapをインストールし、form.phpにて読み込みました。

--- companies/views/form.php
+++ companies/views/form.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf=8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.css">
   <title>会社情報の登録</title>
 </head>
 
