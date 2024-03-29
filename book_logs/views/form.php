8dfe78149e0c676794ca8cbbfe8d4415cbbc6832
github yosshi-kataoka <github>
2024-03-30 06:49:15 +0900

    form.phpにてapp.cssを読み込んでBootstrapを適用させました。

--- book_logs/views/form.php
+++ book_logs/views/form.php
@@ -5,6 +5,7 @@
   <meta charset=utf-8>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>読書ログの登録</title>
+  <link rel="stylesheet" href="stylesheets/css/app.css">
 </head>
 
 <body>
