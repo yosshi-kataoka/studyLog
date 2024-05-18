b4be287785bf4a2f4506e13744de5da9f87310f2
github yosshi-kataoka <github>
2024-05-18 16:01:27 +0900

    Bootstrap学習のため、架空のサイトを作成します。トップページのヘッダー、navbarを作成。

---
+++ green_forest_site/views/layout.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="stylesheets/css/app.css">
+  <title><?php echo $title ?></title>
+</head>
+
+<body>
+  <?php include $contents; ?>
+  <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
