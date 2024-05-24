6f55292f382e8bc23bbbb0078a8852e8551cf95e
github yosshi-kataoka <github>
2024-05-25 05:40:45 +0900

    お問い合わせページのcontact.phpの枠組みを作成。

---
+++ green_forest_site/contact.php
@@ -0,0 +1,5 @@
+<?php
+
+$title = '「お問い合わせ|ガーデニングショップGREEN FOREST GARDENING(グリーンフォレストガーデニング)」';
+$contents = __DIR__ . '/views/contact.php';
+include __DIR__ . '/views/layout.php';
---
+++ green_forest_site/views/contact.php
@@ -0,0 +1,25 @@
+<!-- パンくずリスト -->
+<nav aria-label="breadcrumb">
+  (ここにパンくずリストが入ります)
+</nav>
+<!-- メイン -->
+<main>
+  <div class="container">
+    <h2>お問い合わせ</h2>
+    <p>ガーデニングショップ GREEN FOREST GARDENING(グリーンフォレストガーデニング)へのお問い合わせは、こちらのフォームをご利用ください。</p>
+  </div>
+  <!-- お問い合わせフォーム -->
+  <div class="py-3">
+    <div class="container">
+      <h3 class="mb-3">お問い合わせフォーム</h3>
+      <!-- フォーム -->
+      <form>
+        (ここにフォームが入ります)
+      </form>
+
+
+    </div>
+
+  </div>
+
+</main>
