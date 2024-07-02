20a9551c93a93d11335a765607f28218e0185a16
github yosshi-kataoka <github>
2024-07-03 05:19:08 +0900

    お問い合わせ完了画面のcomplete.phpおよびviews/complete.phpを作成。

---
+++ green_forest_site/views/complete.php
@@ -0,0 +1,7 @@
+<!-- 問い合わせ完了のお知らせ -->
+<div class="p-4">
+  <div class="container text-center">
+    <h5 class="pb-3">お問い合わせが完了しました。</h5>
+    <button class="btn btn-primary" onclick="location.href='top.php'">トップページ</button>
+  </div>
+</div>
